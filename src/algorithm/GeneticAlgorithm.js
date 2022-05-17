8c280edf3d8f9b9cffddabfa7d72172f9f3c3d30
Yana Kushnerova <yanakushneriova@gmail.com>
2022-05-17 16:31:29 +0300

    change selection algorithm

--- src/algorithm/GeneticAlgorithm.js
+++ src/algorithm/GeneticAlgorithm.js
@@ -21,49 +21,29 @@ export default class GeneticAlgorithm {
     }
 
     selection() {
-        const offspring = []
+        const bestOffspring = this.population.sort((a, b) => b.getFitness() - a.getFitness())[0];
 
-        this.population.forEach(() => {
-            let i1 = 0
-            let i2 = 0
-            let i3 = 0
-
-            while (i1 === i2 || i1 === i3 || i2 === i3) {
-                i1 = Math.floor(Math.random() * this.populationSize)
-                i2 = Math.floor(Math.random() * this.populationSize)
-                i3 = Math.floor(Math.random() * this.populationSize)
-            }
-
-            // console.log('iteration------------')
-            // console.log('i1 ', this.population[i1], ' i2 ', this.population[i2], ' i3 ', this.population[i3])
-            // console.log('i1 F', this.population[i1].getFitness(), ' i2 F', this.population[i2].getFitness(), ' i3 F', this.population[i3].getFitness())
-            const bestFitness = Math.max(this.population[i1].getFitness(), this.population[i2].getFitness(), this.population[i3].getFitness())
-            offspring.push(this.population.find(chromosome => chromosome.getFitness() === bestFitness))
-        })
-        
-        // console.log(offspring)
-        return offspring
+        this.population[this.population.length - 1] = bestOffspring;
     }
 
-    crossover(population) {
-        for (let i = 0; i < population.length; i += 2) {
+    crossover() {
+        for (let i = 0; i < this.population.length; i += 2) {
             if (Math.random() <= this.crossoverProbability) {
                 const split = Math.floor(Math.random() * (this.chromosomeSize))
-                const firstChromosomePart = [population[i].chromosome.slice(0, split), population[i].chromosome.slice(split)]
-                const secondChromosomePart = [population[i + 1].chromosome.slice(0, split), population[i + 1].chromosome.slice(split)]
+                const firstChromosomePart = [this.population[i].chromosome.slice(0, split), this.population[i].chromosome.slice(split)]
+                const secondChromosomePart = [this.population[i + 1].chromosome.slice(0, split), this.population[i + 1].chromosome.slice(split)]
 
-                population[i].chromosome = firstChromosomePart[0].concat(secondChromosomePart[1])
-                population[i + 1].chromosome = secondChromosomePart[0].concat(firstChromosomePart[1])
-
-                // console.log("i chr", this.population[i].chromosome)
-                // console.log("I + 1 chr", this.population[i + 1].chromosome)
+                this.population[i].chromosome = firstChromosomePart[0].concat(secondChromosomePart[1])
+                this.population[i + 1].chromosome = secondChromosomePart[0].concat(firstChromosomePart[1])
             }
         }
     }
 
     mutation() {
         for (let i = 0; i < this.populationSize; i++) {
-            if (Math.random() <= this.mutationProbability) {
+            let chance = this.mutationProbability;
+            if (this.population[i].getFitness() > this.meanFitnessValues[this.meanFitnessValues.length - 1]) chance **= 2;
+            if (Math.random() <= chance) {
                 for (let j = 0; j < this.chromosomeSize; j++) {
                     if (Math.random() < this.mutationProbability) {
                         this.population[i].chromosome[j] === 0 ? this.population[i].chromosome[j] = 1 : this.population[i].chromosome[j] = 0
@@ -75,8 +55,8 @@ export default class GeneticAlgorithm {
 
     step() {
         this.generationCounter += 1
-        const offspring = this.selection()
-        this.crossover(offspring)
+        this.selection()
+        this.crossover()
         this.mutation()
 
         this.currentFitnessValues = this.population.map(individual => individual.getFitness());
